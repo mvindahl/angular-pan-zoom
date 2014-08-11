eb76ea7f92211ee324db6753f57dfa2eb53496e5
Martin Vindahl Olsen <mvindahl@gmail.com>
2014-08-11 11:54:13 +0200

    using karma directly from npm test

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,20 +1,31 @@
-module.exports = function(config) {
-	config.set({
-		basePath : '../',
+module.exports = function (config) {
+    config.set({
+        basePath: '../',
 
-		autoWatch : true,
-		singleRun: false,
-		exclude : [],
+        autoWatch: true,
+        singleRun: false,
+        exclude: [],
 
-		frameworks : [ 'jasmine' ],
+        frameworks: ['jasmine'],
 
-		browsers : [ 'Chrome' ], //[ 'PhantomJS', 'Chrome', 'Firefox', 'Safari'],
+        // list of files / patterns to load in the browser
+        files: [
+            'bower_components/jQuery/dist/jquery.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/hamsterjs/hamster.js',
+            'bower_components/angular-mousewheel/mousewheel.js',
+            'scripts/directives/*.js',
+            'test/unit/*.js'
+        ],
 
-		plugins : [ 'karma-junit-reporter', 'karma-chrome-launcher',
-				'karma-firefox-launcher', 'karma-jasmine', 'karma-coverage', 'karma-phantomjs-launcher' ],
-		junitReporter : {
-			outputFile : 'test_out/unit.xml',
-			suite : 'unit'
-		}
-	});
+        browsers: ['Chrome'], //[ 'PhantomJS', 'Chrome', 'Firefox', 'Safari'],
+
+        plugins: ['karma-junit-reporter', 'karma-chrome-launcher',
+    'karma-firefox-launcher', 'karma-jasmine', 'karma-coverage', 'karma-phantomjs-launcher'],
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
+    });
 };
