6f77c288b4716d5139b553c80f5573c08c9ca5dc
Martin Vindahl Olsen <mvindahl@gmail.com>
2015-04-24 08:51:55 +0200

    bumped version to 1.0.8 and rebuilt

--- bin/panzoom.js
+++ bin/panzoom.js
@@ -1,5 +1,5 @@
 /*!
- AngularJS pan/zoom v1.0.7
+ AngularJS pan/zoom v1.0.8
  @license: MIT
  Github: https://github.com/mvindahl/angular-pan-zoom
 */
@@ -448,8 +448,8 @@ function ($document, PanZoomService) {
                                 }
 
                                 var clickPoint = {
-                                    x: $event.pageX - frameElement.offset().left,
-                                    y: $event.pageY - frameElement.offset().top
+                                    x: $event.originalEvent.pageX - frameElement.offset().left,
+                                    y: $event.originalEvent.pageY - frameElement.offset().top
                                 };
 
                                 if (sign < 0) {
